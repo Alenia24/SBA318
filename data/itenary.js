0a4dadd4d8c95e3ef19722b6ed22fd5c63f33d60
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-10 12:13:33 -0400

    Create data for itenary

---
+++ data/itenary.js
@@ -0,0 +1,300 @@
+const activity = [
+  {
+    id: 1,
+    tripId: 1,
+    name: "Snorkeling in Nusa Penida",
+    duration: "4 hours",
+    location: "Nusa Penida, Bali",
+    description:
+      "Explore the clear waters and vibrant coral reefs of Nusa Penida. Swim alongside colorful fish and sea turtles in one of Bali's most famous snorkeling spots.",
+  },
+  {
+    id: 2,
+    tripId: 1,
+    name: "Visiting Uluwatu Temple",
+    duration: "3 hours",
+    location: "Uluwatu, Bali",
+    description:
+      "Experience the stunning cliffs and the majestic Uluwatu Temple, which is perched on the edge of a 70-meter-high cliff overlooking the ocean.",
+  },
+  {
+    id: 3,
+    tripId: 1,
+    name: "Hiking Mount Batur",
+    duration: "6 hours",
+    location: "Mount Batur, Bali",
+    description:
+      "Embark on an early morning hike to the summit of Mount Batur to catch a breathtaking sunrise over Bali's landscape.",
+  },
+  {
+    id: 4,
+    tripId: 1,
+    name: "Balinese Cooking Class",
+    duration: "4 hours",
+    location: "Ubud, Bali",
+    description:
+      "Learn to prepare traditional Balinese dishes using fresh local ingredients with a hands-on cooking class from an expert chef.",
+  },
+  {
+    id: 5,
+    tripId: 1,
+    name: "Balinese Cooking Class",
+    duration: "4 hours",
+    location: "Ubud, Bali",
+    description:
+      "Learn to prepare traditional Balinese dishes using fresh local ingredients with a hands-on cooking class from an expert chef.",
+  },
+  {
+    id: 6,
+    tripId: 1,
+    name: "Spa Day at Resort",
+    duration: "3 hours",
+    location: "Resort Spa, Bali",
+    description:
+      "Relax and rejuvenate with a traditional Balinese spa experience, including massages, facials, and body treatments.",
+  },
+  {
+    id: 7,
+    tripId: 2,
+    name: "Wine Tasting in Bordeaux",
+    duration: "2 hours",
+    location: "Bordeaux, France",
+    description:
+      "Indulge in a wine tasting experience in Bordeaux, one of the world’s premier wine regions. Sample exquisite wines paired with local delicacies.",
+  },
+  {
+    id: 8,
+    tripId: 2,
+    name: "Exploring the Colosseum",
+    duration: "2 hours",
+    location: "Rome, Italy",
+    description:
+      "Step back in time as you explore the ancient Roman Colosseum, one of the greatest architectural marvels in history.",
+  },
+  {
+    id: 9,
+    tripId: 2,
+    name: "Driving along the German Autobahn",
+    duration: "1 day",
+    location: "Germany",
+    description:
+      "Experience the thrill of driving on the famous German Autobahn with no speed limits, passing through beautiful countryside and charming villages.",
+  },
+  {
+    id: 10,
+    tripId: 2,
+    name: "Visiting the Eiffel Tower",
+    duration: "3 hours",
+    location: "Paris, France",
+    description:
+      "Take in panoramic views of Paris from the iconic Eiffel Tower. Visit the observation deck and enjoy the beauty of the city from above.",
+  },
+  {
+    id: 11,
+    tripId: 2,
+    name: "Cruising along the Rhine River",
+    duration: "4 hours",
+    location: "Rhine Valley, Germany",
+    description:
+      "Relax on a scenic cruise down the Rhine River, passing by medieval castles, vineyards, and picturesque villages.",
+  },
+  {
+    id: 12,
+    tripId: 3,
+    name: "Visit the Meiji Shrine",
+    duration: "3 hours",
+    location: "Shibuya, Tokyo",
+    description:
+      "Start your day with a visit to the Meiji Shrine, one of Tokyo’s most important Shinto shrines. Take a peaceful walk through the surrounding forested area.",
+  },
+  {
+    id: 13,
+    tripId: 3,
+    name: "Explore Asakusa & Senso-ji Temple",
+    duration: "4 hours",
+    location: "Asakusa, Tokyo",
+    description:
+      "Visit the historic Asakusa district and explore the ancient Senso-ji Temple, Tokyo's oldest temple. Dont forget to shop for souvenirs at Nakamise Street.",
+  },
+  {
+    id: 14,
+    tripId: 3,
+    name: "Day Trip to Nikko",
+    duration: "8 hours",
+    location: "Nikko, Japan",
+    description:
+      "Take a day trip to Nikko to visit the UNESCO World Heritage site, Toshogu Shrine, and enjoy the beautiful natural surroundings of Nikko National Park.",
+  },
+  {
+    id: 15,
+    tripId: 3,
+    name: "Visit Tokyo Skytree",
+    duration: "3 hours",
+    location: "Sumida, Tokyo",
+    description:
+      "Visit the Tokyo Skytree, the tallest tower in Japan, and enjoy breathtaking panoramic views of the city and Mount Fuji (on clear days).",
+  },
+  {
+    id: 16,
+    tripId: 3,
+    name: "Shinjuku Gyoen National Garden",
+    duration: "3 hours",
+    location: "Shinjuku, Tokyo",
+    description:
+      "Spend a relaxing morning exploring the Shinjuku Gyoen National Garden, one of Tokyo’s most beautiful parks, blending traditional Japanese, English, and French garden landscapes.",
+  },
+  {
+    id: 17,
+    tripId: 3,
+    name: "Visit Fushimi Inari Taisha",
+    duration: "3 hours",
+    location: "Kyoto, Japan",
+    description:
+      "Start the day with a visit to Fushimi Inari Taisha, famous for its thousands of vermilion torii gates that lead up the mountain. Hike up to the shrine for a scenic view of Kyoto.",
+  },
+  {
+    id: 18,
+    tripId: 3,
+    name: "Explore Arashiyama Bamboo Grove",
+    duration: "4 hours",
+    location: "Arashiyama, Kyoto",
+    description:
+      "Walk through the mystical Arashiyama Bamboo Grove, followed by a visit to the nearby Tenryu-ji Temple and the scenic Togetsukyo Bridge.",
+  },
+
+  {
+    id: 19,
+    tripId: 4,
+    name: "Wildlife Viewing",
+    duration: "3 hours",
+    location: "Denali National Park, Alaska",
+    description:
+      "Venture into the Alaskan wilderness to spot wildlife such as bears, moose, wolves, and eagles in their natural habitat.",
+  },
+  {
+    id: 20,
+    tripId: 4,
+    name: "Hiking in Denali National Park",
+    duration: "6 hours",
+    location: "Denali National Park, Alaska",
+    description:
+      "Hike through the rugged terrain of Denali National Park, with breathtaking views of Denali Mountain, the tallest peak in North America.",
+  },
+  {
+    id: 21,
+    tripId: 4,
+    name: "Northern Lights Viewing",
+    duration: "2 hours",
+    location: "Fairbanks, Alaska",
+    description:
+      "Catch a glimpse of the ethereal Northern Lights dancing across the Alaskan night sky in one of the best spots for aurora viewing.",
+  },
+  {
+    id: 22,
+    tripId: 4,
+    name: "River Rafting",
+    duration: "4 hours",
+    location: "Alaska",
+    description:
+      "Take a thrilling white-water rafting trip down one of Alaska's wild rivers, navigating rapids and enjoying the stunning scenery.",
+  },
+  {
+    id: 23,
+    tripId: 4,
+    name: "Dog Sledding Tour",
+    duration: "2 hours",
+    location: "Anchorage, Alaska",
+    description:
+      "Experience the thrill of a dog sledding adventure, guided by expert mushers who will take you through the Alaskan wilderness on a sled pulled by huskies.",
+  },
+  {
+    id: 24,
+    tripId: 5,
+    name: "Snorkeling at Baby Beach",
+    duration: "2 hours",
+    location: "Baby Beach, Aruba",
+    description:
+      "Dive into crystal-clear waters and explore colorful coral reefs at Baby Beach, one of the best snorkeling spots on the island.",
+  },
+  {
+    id: 25,
+    tripId: 5,
+    name: "Sunset Cruise",
+    duration: "3 hours",
+    location: "Aruba Waters",
+    description:
+      "Set sail on a relaxing sunset cruise along the coast of Aruba, enjoying stunning views of the sun setting over the Caribbean Sea.",
+  },
+  {
+    id: 26,
+    tripId: 5,
+    name: "Visiting Arikok National Park",
+    duration: "4 hours",
+    location: "Arikok National Park, Aruba",
+    description:
+      "Explore the natural beauty of Arikok National Park, which features rugged landscapes, hidden caves, and unique wildlife endemic to Aruba.",
+  },
+  {
+    id: 27,
+    tripId: 5,
+    name: "Relaxing on Eagle Beach",
+    duration: "Whole day",
+    location: "Eagle Beach, Aruba",
+    description:
+      "Spend the day on one of the most beautiful beaches in the Caribbean, relaxing on soft white sand and swimming in warm turquoise waters.",
+  },
+  {
+    id: 28,
+    tripId: 5,
+    name: "Island Tour",
+    duration: "5 hours",
+    location: "Aruba",
+    description:
+      "Take a guided tour of Aruba, visiting its top landmarks, including the California Lighthouse and natural rock formations, and learn about the island’s history and culture.",
+  },
+  {
+    id: 29,
+    tripId: 6,
+    name: "Bungee Jumping in Queenstown",
+    duration: "2 hours",
+    location: "Queenstown, New Zealand",
+    description:
+      "Get your adrenaline pumping with a thrilling bungee jump from the famous Kawarau Bridge, one of the worlds most iconic bungee jumping locations.",
+  },
+  {
+    id: 30,
+    tripId: 6,
+    name: "Glowworm Caves Tour in Rotorua",
+    duration: "3 hours",
+    location: "Rotorua, New Zealand",
+    description:
+      "Explore the mesmerizing glowworm caves in Rotorua, where thousands of bioluminescent glowworms light up the underground world.",
+  },
+  {
+    id: 31,
+    tripId: 6,
+    name: "Hiking the Tongariro Alpine Crossing",
+    duration: "7 hours",
+    location: "Tongariro National Park, New Zealand",
+    description:
+      "Take on one of New Zealands most famous day hikes, the Tongariro Alpine Crossing, which offers stunning views of volcanoes, emerald lakes, and alpine meadows.",
+  },
+  {
+    id: 32,
+    tripId: 6,
+    name: "Milford Sound Cruise",
+    duration: "2 hours",
+    location: "Milford Sound, New Zealand",
+    description:
+      "Cruise through the stunning Milford Sound, surrounded by towering cliffs and pristine waters, while spotting dolphins and seals.",
+  },
+  {
+    id: 33,
+    tripId: 6,
+    name: "Visit Hobbiton",
+    duration: "4 hours",
+    location: "Hobbiton, New Zealand",
+    description:
+      "Step into the magical world of Middle-earth with a guided tour of Hobbiton, the iconic movie set from 'The Lord of the Rings' and 'The Hobbit.'",
+  },
+];
