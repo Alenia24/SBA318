82e6f50cb50e24cf8e36b4c0de4573944d7be2fe
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-11 15:07:27 -0400

    Create a route for id and create a get and patch method in passengers

--- routes/passengers.js
+++ routes/passengers.js
@@ -12,20 +12,53 @@ const error = require("../utilities/error");
 //    res.json(`Passenger router`)
 //})
 
-router.route("/")
-.post((req, res) => {
-    // Check if all requirements are included to create a passenger
-    if(req.body.tripId && req.body.name && req.body.email && req.body.telephone && req.body.address && req.body.emergencyContact && req.body.passportNumber) {
-        // Create the passenger
-        const passenger = {
-            id: passengers[passengers.length - 1].id + 1,
-            ...req.body,
+router.route("/").post((req, res) => {
+  // Check if all requirements are included to create a passenger
+  if (
+    req.body.tripId &&
+    req.body.name &&
+    req.body.email &&
+    req.body.telephone &&
+    req.body.address &&
+    req.body.emergencyContact &&
+    req.body.passportNumber
+  ) {
+    // Create the passenger
+    const passenger = {
+      id: passengers[passengers.length - 1].id + 1,
+      ...req.body,
+    };
+    // Add the passenger to passengers data array
+    passengers.push(passenger);
+    //Display the created passenger
+    res.json(passenger);
+  } else res.json({ Error: "Insufficient Data" });
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    const passenger = passengers.find((p) => p.id == req.params.id);
+
+    if (passenger) res.json(passenger);
+    else next();
+  })
+  .patch((req, res, next) => {
+    // Find the passenger
+    const passenger = passengers.find((p, i) => {
+      // If the trip exists
+      if (p.id == req.params.id) {
+        //Iterate over the req.body
+        for (const key in req.body) {
+          // Update
+          passengers[i][key] = req.body[key];
         }
-        // Add the passenger to passengers data array
-        passengers.push(passenger);
-        //Display the created passenger
-        res.json(passenger);
-    } else res.json({Error: "Insufficient Data"})
-})
+        return true;
+      }
+    });
+    // Display the update review if the review exists
+    if (passenger) res.json(passenger);
+    else next();
+  });
 
 module.exports = router;
