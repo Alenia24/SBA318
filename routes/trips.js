da279b5eb44db65e7c465b75f599d0429fda5b7c
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-10 20:34:00 -0400

    Create post route to create a trip

--- routes/trips.js
+++ routes/trips.js
@@ -9,7 +9,25 @@ const error = require("../utilities/error");
 
 router.route("/")
 .get((req, res) => {
+    // Display all trips
     res.json(trips)
+}).post((req, res) => {
+    // Check if all requirements are included to create a trip
+    if(req.body.tripName && req.body.destination && req.body.startDate && req.body.endDate && req.body.price) {
+        // Check if a trip Name already exists
+        if(trips.find((t) => t.tripName === req.body.tripName)) {
+            return res.json({ Error: "Trip Name already taken" });
+        }
+        //Create the trip
+        const trip = {
+            id: trips[trips.length - 1].id + 1,
+            ...req.body
+        }
+        //Add the trip to trips data
+        trips.push(trip);
+        // Display the created trip
+        res.json(trip);
+    }else res.json({Error: "Insufficient Data"})
 })
 
 module.exports = router;
