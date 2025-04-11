413bacbb8a8fa1d832e6bede02cc5b3a6f0d253f
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-11 14:59:28 -0400

    Create a route for id and create a get and patch method in itenaries

--- routes/itenaries.js
+++ routes/itenaries.js
@@ -29,4 +29,29 @@ router.route("/")
     } else res.json({Error: "Insufficient Data"})
 })
 
+router.route("/:id")
+.get((req, res) => {
+    const itenary = itenaries.find((i) => i.id == req.params.id);
+
+    if(itenary) res.json(itenary)
+    else next();
+})
+.patch((req, res, next) => {
+  // Find the titenary
+  const itenary = itenaries.find((i, j) => {
+    // If the trip exists
+    if (i.id == req.params.id) {
+      //Iterate over the req.body
+      for (const key in req.body) {
+        // Update
+        itenaries[j][key] = req.body[key];
+      }
+      return true;
+    }
+  });
+  // Display the update review if the review exists
+  if (itenary) res.json(itenary);
+  else next();
+});
+
 module.exports = router;
