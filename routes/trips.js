70996ea95beabdc0e37d2c651a83e1b383f42123
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-04-11 14:50:33 -0400

    Create a route for id and create a patch method in trips

--- routes/trips.js
+++ routes/trips.js
@@ -7,27 +7,53 @@ const trips = require("../data/trips");
 // import error handling middleware
 const error = require("../utilities/error");
 
-router.route("/")
-.get((req, res) => {
+router
+  .route("/")
+  .get((req, res) => {
     // Display all trips
-    res.json(trips)
-}).post((req, res) => {
+    res.json(trips);
+  })
+  .post((req, res) => {
     // Check if all requirements are included to create a trip
-    if(req.body.tripName && req.body.destination && req.body.startDate && req.body.endDate && req.body.price) {
-        // Check if a trip Name already exists
-        if(trips.find((t) => t.tripName === req.body.tripName)) {
-            return res.json({ Error: "Trip Name already taken" });
-        }
-        //Create the trip
-        const trip = {
-            id: trips[trips.length - 1].id + 1,
-            ...req.body
-        }
-        //Add the trip to trips data
-        trips.push(trip);
-        // Display the created trip
-        res.json(trip);
-    }else res.json({Error: "Insufficient Data"})
-})
+    if (
+      req.body.tripName &&
+      req.body.destination &&
+      req.body.startDate &&
+      req.body.endDate &&
+      req.body.price
+    ) {
+      // Check if a trip Name already exists
+      if (trips.find((t) => t.tripName === req.body.tripName)) {
+        return res.json({ Error: "Trip Name already taken" });
+      }
+      //Create the trip
+      const trip = {
+        id: trips[trips.length - 1].id + 1,
+        ...req.body,
+      };
+      //Add the trip to trips data
+      trips.push(trip);
+      // Display the created trip
+      res.json(trip);
+    } else res.json({ Error: "Insufficient Data" });
+  });
 
+router.route("/:id")
+.patch((req, res, next) => {
+  // Find the trip
+  const trip = trips.find((t, i) => {
+    // If the trip exists
+    if (t.id == req.params.id) {
+      //Iterate over the req.body
+      for (const key in req.body) {
+        // Update
+        trips[i][key] = req.body[key];
+      }
+      return true
+    }
+  });
+  // Display the update review if the review exists
+  if (trip) res.json(trip);
+  else next();
+});
 module.exports = router;
